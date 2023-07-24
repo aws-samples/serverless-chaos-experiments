5167dfa18a0e8d43d67dbde6db4fb3f772a801ee
Andre Stoll <stoandru@amazon.ch>
2023-07-24 20:06:02 +0200

    Write messages in batches

--- chaos_toolkit/simulate_load.sh
+++ chaos_toolkit/simulate_load.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
 
 # replace with your own values from the CF outputs
-SQS_URL='https://sqs.eu-central-1.amazonaws.com/057611153267/chaosLambda-TriggerQueue'
+# AWS region and SQS queue URL
+AWS_REGION="eu-central-1"
+SQS_QUEUE_URL='https://sqs.eu-central-1.amazonaws.com/057611153267/chaosLambda-TriggerQueue'
 
-echo Starting the experiment
-# aws sqs send-message --queue-url $SQS_URL --message-body "IOT-1 Temp: 51C"
+# Number of messages to generate and send in each batch
+BATCH_SIZE=10
 
-for index in {1..50}
-do
-   echo "Sending message nr. $index"
-   aws sqs send-message --queue-url $SQS_URL --message-body "Message nr: $index" > /dev/null
-done
+# Function to generate a single message
+generate_message() {
+  # Modify this function to generate your custom messages
+  # For example, you can use random data or data from external sources
+  echo "Message: $(date +%s)"
+}
+
+# Function to send messages in batches to SQS
+send_batch_messages() {
+  local batch=()
+  local count=0
+
+  while IFS= read -r message; do
+    batch+=("{\"Id\":\"$count\",\"MessageBody\":\"$message\"}")
+    count=$((count + 1))
+
+    if [ ${#batch[@]} -eq $BATCH_SIZE ]; then
+      aws sqs send-message-batch --queue-url "$SQS_QUEUE_URL" --entries "${batch[@]}" --region "$AWS_REGION"
+      batch=()
+    fi
+  done
+
+  if [ ${#batch[@]} -gt 0 ]; then
+    aws sqs send-message-batch --queue-url "$SQS_QUEUE_URL" --entries "${batch[@]}" --region "$AWS_REGION"
+  fi
+}
+
+# Main script
+main() {
+  echo "Generating and sending messages to SQS queue..."
+  for ((i = 1; i <= 50; i++)); do
+    generate_message
+  done | send_batch_messages
+
+  echo "All messages sent successfully."
+}
+
+main
