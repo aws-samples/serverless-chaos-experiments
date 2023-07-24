89bea68b7fe681cd9a498ffe65a1d459c68df4cc
André Stoll <andre.stoll@hotmail.com>
2023-07-24 09:46:45 +0200

    Chaos lambda with experiments

--- chaos_toolkit/run-code-manipulation-experiment.sh
+++ chaos_toolkit/run-code-manipulation-experiment.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 AWS_REGION="eu-central-1"
+FUNCTION_NAME="ServerlessChaosStack-chaos-experiment"
 PARAMETER_NAME="chaoslambda.config"
 ALARM_NAME="TooManyLambdaInvocationThrottles"
 CHAOS_TYPE="status_code" # latency, exception or status_code
@@ -11,7 +12,7 @@ EXCEPTION_MSG="This is chaos at its best."
 RATE="1" # value between 0 and 1
 DURATION=60
 
-export AWS_REGION PARAMETER_NAME ALARM_NAME CHAOS_TYPE ENABLED DELAY ERROR_CODE EXCEPTION_MSG RATE DURATION
+export AWS_REGION FUNCTION_NAME PARAMETER_NAME ALARM_NAME CHAOS_TYPE ENABLED DELAY ERROR_CODE EXCEPTION_MSG RATE DURATION
 
 chaos run --hypothesis-strategy=continuously \
           --hypothesis-frequency=10 \
--- chaos_toolkit/simulate_load.sh
+++ chaos_toolkit/simulate_load.sh
@@ -6,8 +6,8 @@ SQS_URL='https://sqs.eu-central-1.amazonaws.com/057611153267/ChaosLambdaTriggerQ
 
 # aws sqs send-message --queue-url $SQS_URL --message-body "IOT-1 Temp: 51C"
 
-for index in {1..250}
+for index in {1..11}
 do
-   echo "Sending message batch nr. $index"
+   echo "Sending message nr. $index"
    aws sqs send-message --queue-url $SQS_URL --message-body "Message nr: $index" > /dev/null
 done
