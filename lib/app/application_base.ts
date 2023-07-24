021ea855492f5ec87d1bf19c0bd8f2bd45f7df6b
Andre Stoll <stoandru@amazon.ch>
2023-07-24 14:09:38 +0200

    Refactoring

--- lib/app/application_base.ts
+++ lib/app/application_base.ts
@@ -1,4 +1,4 @@
-import {CfnOutput, Duration, Stack, StackProps} from 'aws-cdk-lib';
+import {CfnOutput, Duration, Stack} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
