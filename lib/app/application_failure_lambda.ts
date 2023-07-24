0e4dbbaa6e907c895a52b939a8538a5ab1f96f8d
Andre Stoll <stoandru@amazon.ch>
2023-07-24 18:19:47 +0200

    Fix Nodejs bundling

--- lib/app/application_failure_lambda.ts
+++ lib/app/application_failure_lambda.ts
@@ -17,35 +17,18 @@ export class ApplicationFailureLambda extends Stack {
 
         // ------------------- LAMBDA FUNCTION -------------------------------
 
-        // const chaosExperimentFn = new lambda.Function(this, 'chaosLambdaExperiment', {
-        //     handler: 'lambda_function.lambda_handler',
-        //     functionName: 'Lambda_with_Failure_Lambda',
-        //     description: 'Performs simple chaos experiments using the failure_lambda library',
-        //     code: lambda.Code.fromAsset('lib/app/resources/lambda/failure-lambda', {
-        //
-        //     }),
-        //     environment: {
-        //         FAILURE_INJECTION_PARAM: chaosParameterName,
-        //     },
-        //     runtime: lambda.Runtime.NODEJS_18_X,
-        //     timeout: Duration.seconds(25),
-        //     tracing: lambda.Tracing.ACTIVE
-        // });
-
-        const chaosExperimentFn = new lambdaNode.NodejsFunction(this, "failureLambdaFunction", {
-                runtime: lambda.Runtime.NODEJS_18_X,
-                handler: 'handler',
-                entry: `${__dirname}/resources/lambda/failure-lambda/lambda_function.js`,
+        const chaosExperimentFn = new lambdaNode.NodejsFunction(
+            this,
+            'failureLambdaFunction', {
+                functionName: 'Lambda_with_Failure_Lambda',
+                entry: `${__dirname}/resources/lambda/failure-lambda/index.js`,
+                description: 'Performs simple chaos experiments using the failure_lambda library',
+                timeout: Duration.seconds(25),
+                tracing: lambda.Tracing.ACTIVE,
                 environment: {
                     FAILURE_INJECTION_PARAM: chaosParameterName,
                 },
-            bundling: {
-                externalModules: [
-                    'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
-                ],
-            },
-            }
-        );
+        });
 
         new ApplicationBase(
             this,
