18f4477592dc5667c0d5771f746ab68d5496c522
Andre Stoll <stoandru@amazon.ch>
2023-10-31 22:05:05 +0100

    remove cdk_nag

--- cdk/bin/serverless_chaos.ts
+++ cdk/bin/serverless_chaos.ts
@@ -3,11 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { DemoOne } from '../lib/demos/1_one/demo_one';
 import { DemoTwo } from '../lib/demos/2_two/demo_two';
 import { DemoThree } from '../lib/demos/3_three/demo_three';
-import { AwsSolutionsChecks } from 'cdk-nag'
-import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
 
 new DemoOne(app, 'ServerlessChaos-Demo-1');
 new DemoTwo(app, 'ServerlessChaos-Demo-2');
